5c131e947b65c24151485781f1872facaa3dc845
Marcin Mika <marcinmika@hotmail.com>
2017-06-26 18:41:01 +0200

    przyciski

--- js/first-js.js
+++ js/first-js.js
@@ -2,36 +2,63 @@
 /*wyzwolenie przy zaladowaniu strony*/
 $(document).ready(function () {
     /*funkcja zmienia wysokosc i szerokości wszystkich sekcji */
+    var sectionCount = $("section").length;
+    
     function fixSectionWidth() {
-        var sectionCount = $("section").length;
+        
+        
         $("section").css({
             "height": "100vh",
             "width": "100%"
         });
-   /*funcja ustawia wutomatyczne ustawianie szerokoscie sekcji drugiej*/     
-        var allArticleWidthCount=100*$('article').length + '%';
+        /*funcja ustawia wutomatyczne ustawianie szerokoscie sekcji drugiej*/
+        var allArticleWidthCount = 100 * $('article').length + '%';
         console.log(allArticleWidthCount);
         $("#second-section").css({
             "height": "100vh",
-            "width": allArticleWidthCount 
+            "width": allArticleWidthCount
         });
-        var scrooled = 0;
-        
-        var heighToScroll = $("#second-section").height();
-        
-        $('#scroolDown').on("click", function(){
-            scrooled = scrooled+heighToScroll;
-            $('body').animate({scrollTop: scrooled});
-            
-            console.log('przyciska dziala');
-        })
-
     };
-    /*funkcja    
-        function name(){
-            
-        }*/
+    /*przyciska down scrolluje w dol o wysokosc diva*/    /*przyciska down scrolluje w dol o wysokosc diva*/
+    var scrooled = 0;
+    function buttomScrollUpDown() {
+        var DownEndScroll=heighToScroll* sectionCount;
+        console.log(DownEndScroll);
+        var heighToScroll = $("#second-section").height();
+    /*na klikniecie przycisku dol jest scroll w dol o wysokosc diva*/    
+        $('#scroolDown').on("click", function () {
+            scrooled = scrooled + heighToScroll;
+if(scrooled<heighToScroll* sectionCount);
+            $('body').animate({
+                scrollTop: scrooled
+            });
+        });
+     /*na klikniecie przycisku gora jest scroll w gore o wysokosc diva*/        
+            $('#scroolUp').on("click", function () {
+if(scrooled>0)
+                scrooled = scrooled - heighToScroll;
+            $('body').animate({
+                scrollTop: scrooled
+            });
+        });
+    };
+    
+/*    function buttomScrollLeftRight() {
 
+        
+        var heighToScroll = $("#first-section").width();
+        console.log(heighToScroll);
+        
+        $('#scroolLeft').on("click", function () {
+            scrooled = scrooled + heighToScroll;
+            $('body').animate({
+                scrollTop: scrooled
+            });
+        });
+    };
+
+    console.log(buttomScrollLeftRight());*/
+    console.log(buttomScrollUpDown());
     console.log(fixSectionWidth());
     console.log('koniec');
     console.log($("section").length);
