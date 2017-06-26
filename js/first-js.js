d8e9161db3af58bdb134cd4e8b1205b724148d26
Marcin Mika <marcinmika@hotmail.com>
2017-06-26 17:33:59 +0200

    przechodzenie obrazu

--- js/first-js.js
+++ js/first-js.js
@@ -1,18 +1,40 @@
 'use strict';
 /*wyzwolenie przy zaladowaniu strony*/
-$(document).ready(function(){
-/*funkcja zmienia wysokosc sekcji */
-    function fixSectionWidth(){
-    var sectionCount = $("section").length;
-    $("section").css({"height":"100vh","width":"100%"});
-    };
-/*funkcja    
-    function name(){
+$(document).ready(function () {
+    /*funkcja zmienia wysokosc i szerokości wszystkich sekcji */
+    function fixSectionWidth() {
+        var sectionCount = $("section").length;
+        $("section").css({
+            "height": "100vh",
+            "width": "100%"
+        });
+   /*funcja ustawia wutomatyczne ustawianie szerokoscie sekcji drugiej*/     
+        var allArticleWidthCount=100*$('article').length + '%';
+        console.log(allArticleWidthCount);
+        $("#second-section").css({
+            "height": "100vh",
+            "width": allArticleWidthCount 
+        });
+        var scrooled = 0;
         
-    }*/
-    
-console.log(fixSectionWidth());
-console.log('koniec');
-console.log($("section").length);
+        var heighToScroll = $("#second-section").height();
+        
+        $('#scroolDown').on("click", function(){
+            scrooled = scrooled+heighToScroll;
+            $('body').animate({scrollTop: scrooled});
+            
+            console.log('przyciska dziala');
+        })
+
+    };
+    /*funkcja    
+        function name(){
+            
+        }*/
+
+    console.log(fixSectionWidth());
+    console.log('koniec');
+    console.log($("section").length);
+    console.log($('article').length);
 
 });
